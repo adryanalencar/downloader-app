3b12276082c0e3625816e309481f58dd6ab31e48
Adryan Alencar <adryan.alencar@sevenfox.com.br>
2022-06-25 03:17:30 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,79 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
+import { VideoCard } from './components/VideoCard';
+
+function validateUrl(url) {
+  const regExp = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
+  return regExp.test(url);
+}
+
+function getIdfromUrl(url) {
+  //https://www.youtube.com/watch?v=rNbZ4DD7IPo
+  // extrach v from url
+  var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+  var match = url.match(regExp);
+  if (match && match[2].length == 11) {
+    return match[2];
+  }
+  return null;
+
+}
+
+async function getVideoData(id) {
+  var response = await fetch("https://downloader-api.herokuapp.com/youtube/"+id);
+  var data = await response.json();
+
+  return data;
+}
 
 function App() {
+  const [url, setUrl] = useState("");
+  const [downloading, setDownloading] = useState(false);
+  const [videoData, setVideoData] = useState({});
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='row col-md-12 col-sm-12'>
+          <div className='col-sm-8 offset-sm-2 col-md-6 offset-md-3'>
+            <form>
+              <div className="mb-3">
+                <label for="url-input" className="form-label">Youtube Video URL:</label>
+                <input type="url" value={url} onChange={e => {
+                  setUrl(e.target.value)
+                }} className="form-control" id="url-input" placeholder='https://www.youtube.com/watch?v=rNbZ4DD7IPo' aria-describedby="url-help" />
+                <div id="url-help" className="form-text">Copie o link do vídeo e cole aqui.</div>
+              </div>
+              <button 
+                type="submit"
+                className="btn btn-primary"
+                onClick={e => {
+                  e.preventDefault();
+                  if (validateUrl(url)) {
+                    var id = getIdfromUrl(url);
+                    getVideoData(id).then(data => {
+                      setDownloading(true);
+                      setVideoData(data);
+                    })
+
+                  }else{
+                    alert("URL inválida!");
+                  }
+                }}
+              >Baixar</button>
+            </form>
+          </div>
+        </div>
+        <div className='row col-md-12 m-0'>
+          <div className='col-sm-8 offset-sm-2 col-md-6 offset-md-3'>
+            {downloading ?
+              <VideoCard
+                videoData={videoData}
+                url={url}
+              />
+            : ''}
+          </div>
+        </div>
       </header>
     </div>
   );
---
+++ src/components/VideoCard.js
@@ -0,0 +1,85 @@
+import { useState } from 'react'
+
+async function getDownloadLinks(url){
+    var response = await fetch("https://downloader-api.herokuapp.com/youtube?url="+url);
+    var data = await response.json();
+
+    return data;
+}
+
+const DownloadLinks = (props) => {
+    return (
+        <ul className="list-group list-group-flush">
+            {props.downloadLinks?.map((link, index) => {
+                return(
+                    <li className="list-group-item bg-dark" key={link.format_id}>
+                        <a href={link.url} target="_blank" rel="noopener noreferrer" download={true}>
+                            <span class="badge rounded-pill bg-success">{link.format} - {link.ext}</span>
+                        </a>
+                    </li>
+                )
+            })}
+        </ul>
+    )
+}
+
+const WhatsappButton = (props) => {
+    // react component to unlock download links on share website url
+    return (
+        <div 
+            className="whatsapp-share-button"
+            onClick={() => {
+                setTimeout(() => {
+                    props.onClick();
+                }, 1000);
+            }}    
+        >
+            <a href={`https://api.whatsapp.com/send?text=${window.location.href}`} target="_blank" rel="noopener noreferrer">
+                <img src="https://img.icons8.com/color/48/000000/whatsapp.png" alt="whatsapp" />
+                <span>Compartilhe para Desbloquear</span>
+            </a>
+        </div>
+    )
+}
+
+
+const VideoCard = (props) => {
+    const [downloadLinks, setDownloadLinks] = useState([]);
+    const [lock, setLock] = useState(true);
+
+    return(
+        <div className="card col-md-12 bg-dark">
+            <div className="row g-0">
+                <div className="col-md-4">
+                <img src={props.videoData.video.snippet.thumbnails.maxres.url} className="img-fluid rounded-start" alt="..." />
+                </div>
+                <div className="col-md-8">
+                    <div className="card-body">
+                        <h5 className="card-title">{props.videoData.video.snippet.title}</h5>
+                        <p className="card-text">{props.videoData.video.snippet.description.substr(0, 100) + '...' }</p>
+                    </div>
+                </div>
+                {lock ?
+                    <ul className='list-group list-group-flush'>
+                        <li className="list-group-item bg-dark">
+                            <WhatsappButton onClick={() => {
+                                getDownloadLinks(props.url).then(data => {
+                                    setDownloadLinks(data.video.formats);
+                                    setLock(false);
+                                })
+                            }}/>
+                        </li>
+                    </ul>    
+                    :
+                    <DownloadLinks 
+                        downloadLinks={downloadLinks}
+                    />
+                }
+            </div>
+        </div>
+    )
+}
+
+export {
+    VideoCard
+}
